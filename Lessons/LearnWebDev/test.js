252512e5515ec8d2547dbf2fd35559c78fbdbe13
DominicYoung1 <domminero.14@gmail.com>
2021-01-09 13:37:51 -0600

    Addding the first two and a half into exapmles

---
+++ Lessons/LearnWebDev/test.js
@@ -0,0 +1,16 @@
+const bubblesort = (array, inOrder) => {
+    for (let i=0; i < array.length; i++)  {
+        let foundAnIssue=false
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+const myArray = [10,15,6,52,12,4,2,12]
